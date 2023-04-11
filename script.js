4f5b8f4bff49d484ce92d9c6909777f8da2e7d1d
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-11 01:10:50 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-// a console.log()
+// Console.log()
 
 console.log('This program displays text and consists of a single command');
+
+// Data types
+
+/*
+Number
+Strings
+Boolean
+*/
+
+// Variable
+
+let car = 'MarkX';
+let time = '6:00PM';
