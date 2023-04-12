308e4f5d8118923105a8e002798e15fccf77bd3f
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-12 10:41:00 -0500

    3 files modified, 2 deleted

--- constants.js
+++
@@ -1,8 +0,0 @@
-// constants.js
-// export to use in a different file
-export const monthsSalary = 5000;
-export const isHappy = true;
-
-// My Try
-export const isActive = false;
-export const driverAge = `+${18}`;
