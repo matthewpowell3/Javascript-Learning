d256deeccbea5fa433114671b512c0eae79dcbfc
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-04-12 15:17:10 -0500

    1 file modified

--- script.js
+++ script.js
@@ -4,11 +4,29 @@ let myName;
 myName = 'Matthew';
 console.log(myName);
 
-
-// const are not redefinable 
-const name = 'John'
+// const are not redefinable
+const name = 'John';
 /*
 Can't Work
 name = "Mark"
 */
-console.log(name)
+console.log(name);
+
+const Name = 'Matthew';
+const age = 12;
+const isHoverable = true / false;
+const noValue = undefined;
+// Null is used when you want to clear a variable
+const selectedColor = null;
+
+// Objects
+
+// Right way to group things
+const person = {
+  myRealName: 'Matthew',
+  myRealAge: 14,
+};
+
+// Wrong way to group things
+let myRealName = 'Matthew';
+let myRealAge = 14;
